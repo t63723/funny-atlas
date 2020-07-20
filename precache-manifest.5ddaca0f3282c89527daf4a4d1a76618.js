1bdd4cff7da5e54b7074220eaeff90135e89f99d
Mikhail Katsupko <t63723@gmail.com>
2020-07-20 16:46:54 +0300

    Updates

---
+++ precache-manifest.5ddaca0f3282c89527daf4a4d1a76618.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852d63bb034daf1ba87a",
+    "url": "bundle.css"
+  },
+  {
+    "revision": "852d63bb034daf1ba87a",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "c64beab291de80970aa4887a5a1c9135",
+    "url": "favicon.png"
+  },
+  {
+    "url": "icon_120x120.68b5a1e1b5d618c28c611bad44f4a6dd.png"
+  },
+  {
+    "url": "icon_128x128.0aff1a56a1950f0ec54c48e49bf2fce9.png"
+  },
+  {
+    "url": "icon_152x152.fe346b33d8bf1a214f5750a87e231a41.png"
+  },
+  {
+    "url": "icon_167x167.3ccd0ec52cf09ef36345847e78ccb105.png"
+  },
+  {
+    "url": "icon_180x180.5402b3298bb0f3d8132045b631a20319.png"
+  },
+  {
+    "url": "icon_192x192.9fe9a72e36a0fb1bf1743ac781fce5c1.png"
+  },
+  {
+    "url": "icon_192x192.e1063e8b99f495dfb451e4eb4ef843a4.png"
+  },
+  {
+    "url": "icon_32x32.5baf26b91e1e0ce9f8727ec071f2193a.png"
+  },
+  {
+    "url": "icon_512x512.f5b64ef6cbbb8d106d4fafc6f3b4cf30.png"
+  },
+  {
+    "url": "icon_64x64.df428e3abd55395f40bfe799c1e687bc.png"
+  },
+  {
+    "url": "icon_96x96.cce625f7dac56eede7943d4c0f8c6d44.png"
+  },
+  {
+    "revision": "bd4e5ba96654fc5ef2d2f9bbbd8ffa27",
+    "url": "index.html"
+  },
+  {
+    "url": "manifest.385ecc14d968e7c666cf64918b6005b6.json"
+  }
+]);
